ea313232164510817dc8b680be5239eca97ea128
Abhishek <116707298+Mr-Abhishek0@users.noreply.github.com>
2022-11-12 22:19:40 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,37 @@
+const Orders = [{
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        payMentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        payMentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko F11 Pro Drone ',
+        productNumber: '49347',
+        payMentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96943',
+        payMentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22684',
+        payMentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        payMentStatus: 'Due',
+        shipping: 'Pending',
+    },
+]
